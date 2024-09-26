b2e974bd34479b4a1af9b86e11daf3f8327a5e8b
Matteo Bacci <matteo.bacci@nearform.com>
2024-09-26 13:31:19 +0200

    fixed spacing

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default async function NotFound({
   ctaLabel = "Torna alla home",
 }) {
   return (
-    <section className="flex flex-col justify-center items-center space-x-4 my-4 min-h-full">
+    <section className="flex flex-col justify-center items-center my-4 min-h-full text-center">
       <h2 className="mt-4 font-semibold text-3xl text-secondary">
         Hai fallito il tiro salvezza!
       </h2>
