8a8e2d36d9d8f61f465a9b2552a774abd50a77e3
Matteo Bacci <matteo.bacci@nearform.com>
2024-09-30 09:18:18 +0200

    added caching

--- src/lib/adventures.ts
+++ src/lib/adventures.ts
@@ -2,6 +2,7 @@ import { getBookings } from "@/lib/bookings";
 import { getSheetData } from "@/lib/sheets";
 import { Adventure, SheetRange } from "@/types";
 import crypto from "crypto";
+import { unstable_cache } from "next/cache";
 
 const spreadsheetId = process.env.SHEET_ID as string;
 const adventureRange = process.env.SHEET_ADVENTURE_RANGE as SheetRange;
@@ -17,62 +18,68 @@ function generateAdventureId(
   return hash.digest("hex");
 }
 
-export async function getAdventures(): Promise<Adventure[]> {
-  const adventuresResponse = await getSheetData({
-    spreadsheetId,
-    range: adventureRange,
-  });
-
-  if (!adventuresResponse || adventuresResponse.length === 0) {
-    throw new Error("No data found");
-  }
-
-  const bookings = await getBookings();
-
-  const adventures: Adventure[] = adventuresResponse
-    .slice(1)
-    .map((row: string[]) => {
-      const [
-        timeSlot,
-        title,
-        rules,
-        description,
-        minPlayers,
-        maxPlayers,
-        tableShape,
-        masterName,
-      ] = row;
-
-      const id = generateAdventureId(
-        title,
-        masterName,
-        Number(timeSlot) as Adventure["timeSlot"]
-      );
-
-      const booked = bookings
-        .filter(({ adventureId }) => adventureId === id)
-        .reduce((total, { seats }) => total + seats, 0);
-
-      return {
-        id,
-        timeSlot: Number(timeSlot) as Adventure["timeSlot"],
-        title,
-        rules,
-        description,
-        minPlayers: Number(minPlayers),
-        maxPlayers: Number(maxPlayers),
-        tableShape,
-        masterName,
-        availableSeats: Number(maxPlayers) - booked,
-      };
-    })
-    .sort((a, b) => {
-      if (a.timeSlot === b.timeSlot) {
-        return a.title.localeCompare(b.title);
-      } else {
-        return a.timeSlot - b.timeSlot;
-      }
+export const getAdventures = unstable_cache(
+  async (): Promise<Adventure[]> => {
+    const adventuresResponse = await getSheetData({
+      spreadsheetId,
+      range: adventureRange,
     });
 
-  return adventures;
-}
+    if (!adventuresResponse || adventuresResponse.length === 0) {
+      throw new Error("No data found");
+    }
+
+    const bookings = await getBookings();
+
+    const adventures: Adventure[] = adventuresResponse
+      .slice(1)
+      .map((row: string[]) => {
+        const [
+          timeSlot,
+          title,
+          rules,
+          description,
+          minPlayers,
+          maxPlayers,
+          tableShape,
+          masterName,
+        ] = row;
+
+        const id = generateAdventureId(
+          title,
+          masterName,
+          Number(timeSlot) as Adventure["timeSlot"]
+        );
+
+        const booked = bookings
+          .filter(({ adventureId }) => adventureId === id)
+          .reduce((total, { seats }) => total + seats, 0);
+
+        return {
+          id,
+          timeSlot: Number(timeSlot) as Adventure["timeSlot"],
+          title,
+          rules,
+          description,
+          minPlayers: Number(minPlayers),
+          maxPlayers: Number(maxPlayers),
+          tableShape,
+          masterName,
+          availableSeats: Number(maxPlayers) - booked,
+        };
+      })
+      .sort((a, b) => {
+        if (a.timeSlot === b.timeSlot) {
+          return a.title.localeCompare(b.title);
+        } else {
+          return a.timeSlot - b.timeSlot;
+        }
+      });
+
+    return adventures;
+  },
+  ["get-adventures"],
+  {
+    revalidate: 5 * 60 * 1000, // 5 minutes
+  }
+);
