4876168e1c34c4844d9c3a891f33f49b8f6aa748
Matteo Bacci <matteo.bacci@nearform.com>
2024-10-12 11:40:12 +0200

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,31 +4,34 @@ import logo from "../public/logo.png";
 
 export const Header = () => {
   return (
-    <header className="flex-col bg-base-200 mb-4 rounded-md navbar">
+    <header className="flex flex-col items-center space-y-4 mb-4 w-full">
       <Link href="/">
         <Image
           src={logo}
           alt="Logo Total Party Kon"
           style={{
-            width: "100%",
-            height: "auto",
+            width: "auto",
+            height: "100%",
           }}
-          width={120}
+          height={100}
         />
       </Link>
-      <ul className="space-x-2 font-bold menu menu-horizontal">
-        <li>
-          <Link href="https://www.totalpartykon.it/">Cos'è TPK!</Link>
-        </li>
-        <li>
-          <Link href="https://www.totalpartykon.it/dove-e-quando">
-            Dove e quando?
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.totalpartykon.it/contatti">Contatti</Link>
-        </li>
-      </ul>
+
+      <nav className="bg-base-200 rounded-md navbar">
+        <ul className="flex justify-center space-x-2 font-bold menu menu-horizontal">
+          <li>
+            <Link href="https://www.totalpartykon.it/">Cos&apos;è TPK!</Link>
+          </li>
+          <li>
+            <Link href="https://www.totalpartykon.it/dove-e-quando">
+              Dove e quando?
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.totalpartykon.it/contatti">Contatti</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
