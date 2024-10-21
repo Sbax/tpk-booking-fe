17bfe2207c25c07afee6b6d22bc470544d334525
Matteo Bacci <matteo.bacci@nearform.com>
2024-10-21 10:38:10 +0200

    removed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import { Header } from "@/components/Header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Prenotazioni | TPK! 2024",
@@ -26,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 `}
-      >
+      <body className="p-4 antialiased">
         <Header />
         {children}
       </body>
